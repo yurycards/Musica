7d9432d59512bd8e6e5ae2efa1d051cb9b9c6a3d
yuri cardoso <yurycards@gmail.com>
2025-03-28 20:38:11 -0300

    atividade realizada

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("misterio");
+Console.WriteLine();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(@"Hoje eu não choro mais
+Mais, mais, mais, mais, mais, mais
+Quero você, vem pro pai
+Pai, pai, pai, pai
+Hoje eu não choro mais
+Mais, mais, mais, mais, mais
+Quero você, vem pro pai
+Pai, pai, pai, pai
+Hoje eu não choro mais
+Mais, mais, mais, mais, mais, mais
+Quero você, vem pro pai
+Pai, pai, pai, pai, uh
+Hoje eu não choro mais
+Mais, mais, mais, mais, mais
+Quero você, vem pra mim
+Mim");
+Console.ResetColor();
